9460ead759122dc1052e4848f368bc2eb538fa07
jytyaywymwbayl-afk <jytyaywymwbayl@gmail.com>
2026-07-09 22:23:32 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+function toggleMenu() {
 
+document.getElementById("sidebar").classList.toggle("active");
+
+document.getElementById("overlay").classList.toggle("active");
+
+}
+
+// داده‌ها
+
+let users = JSON.parse(localStorage.getItem("users")) || [];
+
+updateDashboard();
+
+function updateDashboard(){
+
+document.getElementById("usersCount").innerHTML = users.length;
+
+document.getElementById("configsCount").innerHTML = users.length;
+
+document.getElementById("activeCount").innerHTML = users.length;
+
+}
+
+function saveUsers(){
+
+localStorage.setItem("users",JSON.stringify(users));
+
+updateDashboard();
+
+}
+
+// تست
+
+console.log("Shahin Panel Loaded");
